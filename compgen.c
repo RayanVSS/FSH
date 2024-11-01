6b514d1935754d9765125c3ee5bfe9cf2a2a5ad0
Belhasse <rayan.belhassen@etu.u-paris.fr>
2024-11-01 21:42:54 +0100

    correction compgen

--- compgen.c
+++ compgen.c
@@ -5,33 +5,32 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
+#include <errno.h>
 
 int is_executable(const char *path) {
     struct stat sb;
-    if (stat(path, &sb) == 0 && sb.st_mode & S_IXUSR) {
-        return 1;
-    }
-    return 0;
+    return (stat(path, &sb) == 0 && sb.st_mode & S_IXUSR) ? 1 : 0;
 }
 
 // commandes internes
 void list_internal_commands(char *internal_commands[]) {
     for (int i = 0; internal_commands[i] != NULL; i++) {
-        printf("%s\n", internal_commands[i]);
+        write(STDOUT_FILENO, internal_commands[i], strlen(internal_commands[i]));
+        write(STDOUT_FILENO, "\n", 1);
     }
 }
 
-
-int execute_compgen(char *internal_commands[],int argc, char **argv) {
-    // les arguments
+int execute_compgen(char *internal_commands[], int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: compgen -c\n");
+        perror("Usage: compgen -c");
         return 1;
     }
+    
     if (strcmp(argv[1], "-c") == 0) {
         list_internal_commands(internal_commands);
     } else {
         fprintf(stderr, "compgen: option inconnue: %s\n", argv[1]);
+        perror("Erreur");
         return 1;
     }
 
